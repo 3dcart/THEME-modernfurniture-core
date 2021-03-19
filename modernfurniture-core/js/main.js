7ce76bbdd371e782ca3192efd618ce863462bc32
asaghir <asaghir@3dcart.com>
2021-03-19 14:18:01 -0400

    theme updated for ada

--- modernfurniture-core/js/main.js
+++ modernfurniture-core/js/main.js
@@ -67,14 +67,17 @@ function mailinglist_response(form, response) {
     if (response == 1 || response == 3) {
         jQuery('#mailinglist-response .mailinglist-subscribed').removeClass('hidden');
         jQuery('#mailinglist-response').slideDown(300);
+        jQuery('.mailinglist-input').attr( 'aria-invalid', 'false');
     }
     else if (response == -1) {
         jQuery('#mailinglist-response .mailinglist-unsubscribed').removeClass('hidden');
         jQuery('#mailinglist-response').slideDown(300);
+        jQuery('.mailinglist-input').attr( 'aria-invalid', 'false');
     }
     else if (response == 2) {
         jQuery('#mailinglist-response .mailinglist-error').removeClass('hidden');
         jQuery('#mailinglist-response').slideDown(300);
+        jQuery('.mailinglist-input').attr( 'aria-invalid', 'true');
     }
 
     jQuery(form).find('#mailing-btn-txt').removeClass('hidden');
@@ -176,4 +179,17 @@ jQuery(document).ready(function () {
 		jQuery('html, body').animate({ scrollTop: 0 }, 800);
 		return false;
 	});
+});
+jQuery(function () { 
+	jQuery('.nav .dropdown > a').attr("aria-expanded","false");
+	jQuery('.nav .dropdown > a').attr("aria-haspopup","true");
+    jQuery('.nav .dropdown > a').hover(function (e) {
+        var menuItem = jQuery( e.currentTarget );
+
+        if (menuItem.attr( 'aria-expanded') === 'true') {
+            jQuery(this).attr( 'aria-expanded', 'false');
+        } else {
+            jQuery(this).attr( 'aria-expanded', 'true');
+        }
+    });
 });
